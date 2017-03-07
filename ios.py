8a847578ea95754cd169d5ef2a892d50063d9711
hyunmini85 <hyunmini85@gmail.com>
2017-03-07 11:24:31 +0900

    commit

--- ios.py
+++ ios.py
@@ -6,14 +6,6 @@ import codecs
 from util import shell
 from pwn import *
 
-class Util:
-	@staticmethod
-	def randstr(length):
-		randomString=""
-		for i in range(length):
-		    randomString += (str(unichr(random.randint(97,122)))) 
-		return randomString  
-
 def on_message(message, data):
 	if message['type'] == 'error':
 		Logger.warn("[Error] : %s" % message['description'])
@@ -31,39 +23,3 @@ class IOS:
 	def __init__(self):
 		self.scheme = "http"
 		self.host = ""
-
-def get_procinfo(identifier):
-	os = frida.get_usb_device().name
-	if os == 'iPhone':
-		apps = frida.get_usb_device().enumerate_applications()		
-		for app in apps:
-			if app.identifier == identifier:
-				print "yes", app
-			else:
-				print "no",app
-
-#(identifier="com.shinhan.smartcaremgr", name="S알리미")
-
-def main(target_process):
-	pid = frida.get_usb_device().spawn([target_process])
-	session = frida.get_usb_device().attach(pid)
-	#frida.get_usb_device().resume(pid)
-	#for module in session.enumerate_modules():
-	#		print "[+] ",module
-	with codecs.open('./dbi.js','r',encoding='utf8') as f:
-		dbi_js = f.read()
-	script = session.create_script(dbi_js) 
- 
-	script.on('message', on_message)
-	script.load()
-	frida.get_usb_device().resume(pid)
-	sys.stdin.read()
- 
-if __name__ == '__main__':
-	if len(sys.argv) != 2:
-		print "Usage : %s [Identifier]" %  sys.argv[0]
-		print "ex) %s com.shinhan.sbank" %  sys.argv[0]
-		sys.exit()
-	target_process = sys.argv[1]
-	#get_procinfo(target_process)
-	main(target_process)
